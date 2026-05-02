b6a44cc3dae4377226d5f08d94df079091e60467
fares <fares@LAPTOP-7A95A3RT.lan>
2026-05-02 19:52:30 +0400

    Added my name

--- Spring2026_LA1_Repo/src/Driver.java
+++ Spring2026_LA1_Repo/src/Driver.java
@@ -3,6 +3,7 @@ public class Driver {
 
 	public static void main(String[] args) {
 		System.out.println("Khubaib Alam");
+		System.out.println("Fares Awda");
 		System.out.println("Ayman Khalid");
 		System.out.println("YASER SAFAR"); //
 		System.out.println("mahmoud "); //
